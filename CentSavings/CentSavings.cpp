0dc146cf76c190f3ac7bce7003eb2a3f42b8c52f
XixWISExiX <addalooptoit@gmail.com>
2023-11-09 10:54:09 -0600

    Added another matrix for normal values

--- CentSavings/CentSavings.cpp
+++ CentSavings/CentSavings.cpp
@@ -26,7 +26,7 @@ int customRound(int num) {
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    // freopen("input.txt", "r", stdin);
+    freopen("input.txt", "r", stdin);
 
     int n, d, temp;
     cin >> n >> d;
@@ -42,15 +42,18 @@ int main() {
     int N = n+1;
     int D = d+1;
     int dp[D][N][N];
+    int dps[N][N];
 
     for(int i = 0; i < N; i++){
         // dp[0][i][i] = customRound(cents[i]);
         dp[0][i][i] = cents[i];
+        dps[i][i] = cents[i];
     }
 
     for(int i = 1; i < N; i++){
         for(int j = 0; j+i < N; j++){
             // dp[0][j][j+i] = customRound(dp[0][j][j] + dp[0][j+1][j+i]);
+            dps[j][j+i] = dps[j][j] + dps[j+1][j+i];
             dp[0][j][j+i] = dp[0][j][j] + dp[0][j+1][j+i];
         }
     }
@@ -77,7 +80,8 @@ int main() {
                     // dp[k][j][j+i] = min(dp[k][j][j+i], cents[j] + max(customRound(dp[k-1][j][x-1]), dp[k][x][j+i]));
                     // dp[k][j][j+i] = min(customRound(dp[k][j][j+i]) + max(dp[k-1][j][x-1], dp[k][x][j+i]), dp[k][j][j+i] + max(dp[k-1][j][x-1], dp[k][x][j+i]));
                     // dp[k][j][j+i] = min(customRound(dp[k-1][j][j+i]), dp[k-1][j][j+i]);
-                    dp[k][j][j+i] = min(dp[k][j][j+i], customRound(dp[0][j][x-1] + dp[0][x][j+i]));
+                    // dp[k][j][j+i] = min(dp[k][j][j+i], customRound(dp[0][j][x-1] + dp[0][x][j+i]));
+                    dp[k][j][j+i] = min(dp[k][j][j+i], min(customRound(dps[j][x-1] + dps[x][j+i]), customRound(dp[k-1][j][x-1] + dp[0][x][j+i])));
                     // dp[k][j][j+i] = min(dp[k][j][j+i], max(customRound(dp[k-1][j][x-1])+dp[k][x][j+i], dp[k-1][j][x-1]+dp[k][x][j+i]));
                     // dp[k][j][j+i] = min(dp[k][j][j+i], max(customRound(dp[k-1][j][x-1])+dp[k][x][j+i], dp[k-1][j][x-1]+dp[k][x][j+i]));
                     // dp[k][j][j+i] = min(dp[k][j][j+i], min(customRound(dp[k-1][j][x-1]), customRound(dp[k][x][j+i])));
